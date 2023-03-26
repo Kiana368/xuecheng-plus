a6895f81135c6633614d076a96c3c620429c8418
Kiana368 <sijuntao@umich.edu>
2023-03-26 18:25:58 -0400

    nacos搭建网关，minIO分布式文件管理

---
+++ xuecheng-plus-system/xuecheng-plus-system-service/src/main/java/com/xuecheng/system/mapper/DictionaryMapper.java
@@ -0,0 +1,11 @@
+package com.xuecheng.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.system.model.po.Dictionary;
+
+/**
+ * 数据字典 Mapper 接口
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
