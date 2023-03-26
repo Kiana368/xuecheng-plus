41909135bacc17bed97c9fd5452b3e8d1b2acbaf
Kiana368 <sijuntao@umich.edu>
2023-03-26 17:30:40 -0400

    媒资管理

---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/mapper/MqMessageMapper.java
@@ -0,0 +1,11 @@
+package com.xuecheng.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.media.model.po.MqMessage;
+
+/**
+ *  Mapper 接口
+ */
+public interface MqMessageMapper extends BaseMapper<MqMessage> {
+
+}
